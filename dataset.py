8c7c1791e013cac37f7c6beb97c8d350cda910ef
Marco Baturan <marco.baturan@gmail.com>
2018-05-17 18:53:38 +0200

    4º commit

--- dataset.py
+++ dataset.py
@@ -3,14 +3,13 @@ from prettytable import from_csv, PrettyTable
 
 def main():
     """ It's script which trasnlate csv to HTML table.
-
     """
     with open("dataset.csv", "r") as fp:
-        x = from_csv(fp)
-        y = PrettyTable.get_html_string(x)
-        table_html = y
-        html_file=open('table.html','w')
-        html_file=html_file.write(table_html)
+        x = from_csv(fp) # read from csv by PT
+        y = PrettyTable.get_html_string(x) # translate to html, PT
+        table_html = y # store in var
+        html_file=open('table.html','w') # create and write
+        html_file=html_file.write(table_html) # add var html
 
 if __name__ == '__main__':
     main()
